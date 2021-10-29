094ed548de449d14c65917ebc273576e81f4e3a8
Ralf Pannemans <ralf.pannemans@googlemail.com>
2021-10-29 21:29:14 +0200

    Add test class and coverage to pom.xml

---
+++ src/test/java/io/diffblue/javademo/CheckTicTacToeTest.java
@@ -0,0 +1,12 @@
+package io.diffblue.javademo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+
+public class CheckTicTacToeTest {
+
+	public static TicTacToe t = new TicTacToe();
+
+}
